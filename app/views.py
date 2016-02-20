bd075942a4a00d0266412a1a141a4b332e9104cf
tonitiffy <toniannfitzgerald@gmail.com>
2016-02-20 18:48:05 +0000

    updated view.py

--- app/views.py
+++ app/views.py
@@ -28,44 +28,47 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
     
-@app.route('/contact/', methods=["GET", "POST"])
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
     """Render the website's about page."""
     form = ContactForm()
-    def sendemail(fromname,fromaddr,subject,msg):
-        toname = 'Toni-Ann Fitzgerald' 
-        toaddr  = 'toni-annfitzgerald@hotmail.com'
-        message = """From: {} <{}>\nTo: {} <{}>\nSubject: {}\n\n{}"""
-        
-        messagetosend = message.format(
-                                     fromname,
-                                     fromaddr,
-                                     toname,
-                                     toaddr,
-                                     subject,
-                                     msg)
-        
-        # Credentials (if needed)
-        username = 'toniannfitzgerald@gmail.com'
-        password = 'xqgqdjeitwfxdlac'
-        
-        # The actual mail send
-        server = smtplib.SMTP('smtp.gmail.com:587')
-        server.starttls()
-        server.login(username,password)
-        server.sendmail(fromaddr, toaddr, messagetosend)
-        server.quit()
     
     if form.validate_on_submit():
         name = form.name.data
         email = form.email.data
         subject = form.subject.data
-        message = form.message.data
-        sendemail(name,email,subject,message)
+        msg = form.message.data
         
+        sendemail(fromname=name,fromemail=email,fromsubject=subject,msg=msg)
         
     return render_template('contact.html', form=form)
 
+
+def sendemail(fromname,fromemail,fromsubject,msg):
+    toname = 'Toni-Ann Fitzgerald' 
+    toaddr  = 'toni-annfitzgerald@hotmail.com'
+    message = """From: {} <{}>\nTo: {} <{}>\nSubject: {}\n\n{}"""
+    
+    messagetosend = message.format(
+                                 fromname,
+                                 fromemail,
+                                 toname,
+                                 toaddr,
+                                 fromsubject,
+                                 msg)
+    
+    # Credentials (if needed)
+    username = 'toniannfitzgerald@gmail.com'
+    password = 'xqgqdjeitwfxdlac'
+    
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromemail, toaddr, messagetosend)
+    server.quit()
+    return
+        
 
 ###
 # The functions below should be applicable to all Flask apps.
